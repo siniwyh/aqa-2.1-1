09528fba49a7da55bd5c4a8d110ec77c45f05a80
unknown <siniwyh@icloud.com>
2024-07-20 18:28:27 +0700

    fix test

--- src/test/java/OrdercardTest.java
+++ src/test/java/OrdercardTest.java
@@ -9,11 +9,11 @@ class OrdercardTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999/");
-        $("[data-test-id=\"name\"]").setValue("Бо Би");
-        $("[data-test-id=\"phone\"]").setValue("+79998887766");
-        $("data-test-id=\"agreement\"").click();;
-        $("data-test-id=\"order-success\"").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-
+        $("[data-test-id=name] input").setValue("Бо Би");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement]").click();;
+        $(".button__content").click();;
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
 
