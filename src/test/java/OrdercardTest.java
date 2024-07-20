de83e42f02bcea87ddbb667f90dde2ecae661b4e
unknown <siniwyh@icloud.com>
2024-07-20 16:51:57 +0700

    test added

---
+++ src/test/java/OrdercardTest.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class OrdercardTest {
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999/");
+        $("[data-test-id=\"name\"]").setValue("Бо Би");
+        $("[data-test-id=\"phone\"]").setValue("+79998887766");
+        $("data-test-id=\"agreement\"").click();;
+        $("data-test-id=\"order-success\"").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+}
+
